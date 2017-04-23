e2fe6567fddd8a522ad4998cbda33aa73992b71e
houdunwang <2300071698@qq.com>
2017-04-23 19:47:20 +0800

    no message

--- src/WeChatProvider.php
+++ src/WeChatProvider.php
@@ -2,16 +2,22 @@
 
 use houdunwang\framework\build\Provider;
 
-class WeChatProvider extends Provider {
-	//延迟加载
-	public $defer = true;
+class WeChatProvider extends Provider
+{
+    //延迟加载
+    public $defer = true;
 
-	public function boot() {
-	}
+    public function boot()
+    {
+    }
 
-	public function register() {
-		$this->app->single( 'WeChat', function () {
-			return new WeChat();
-		} );
-	}
+    public function register()
+    {
+        $this->app->single(
+            'WeChat',
+            function () {
+                return new WeChat();
+            }
+        );
+    }
 }
