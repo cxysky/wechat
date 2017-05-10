018b4a8b38e8b216fba8025f0a7e8a1ee2ff5abb
houdunwang <2300071698@qq.com>
2017-05-10 12:22:47 +0800

    no message

--- src/WeChat.php
+++ src/WeChat.php
@@ -1,6 +1,5 @@
 <?php namespace houdunwang\wechat;
 
-use houdunwang\config\Config;
 use houdunwang\wechat\build\Base;
 
 /** .-------------------------------------------------------------------
@@ -11,36 +10,41 @@ use houdunwang\wechat\build\Base;
  * |    WeChat: aihoudun
  * | Copyright (c) 2012-2019, www.houdunwang.com. All Rights Reserved.
  * '-------------------------------------------------------------------*/
-class WeChat {
-	//连接
-	protected $link;
+class WeChat
+{
+    //连接
+    protected $link;
 
-	//更改缓存驱动
-	protected function driver() {
-		$this->link = new Base();
+    //更改缓存驱动
+    protected function driver()
+    {
+        $this->link = new Base();
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function __call( $method, $params ) {
-		if ( is_null( $this->link ) ) {
-			$this->driver();
-		}
-		if ( method_exists( $this->link, $method ) ) {
-			return call_user_func_array( [ $this->link, $method ], $params );
-		}
-	}
+    public function __call($method, $params)
+    {
+        if (is_null($this->link)) {
+            $this->driver();
+        }
+        if (method_exists($this->link, $method)) {
+            return call_user_func_array([$this->link, $method], $params);
+        }
+    }
 
-	public static function single() {
-		static $link = null;
-		if ( is_null( $link ) ) {
-			$link = new static();
-		}
+    public static function single()
+    {
+        static $link = null;
+        if (is_null($link)) {
+            $link = new static();
+        }
 
-		return $link;
-	}
+        return $link;
+    }
 
-	public static function __callStatic( $name, $arguments ) {
-		return call_user_func_array( [ static::single(), $name ], $arguments );
-	}
+    public static function __callStatic($name, $arguments)
+    {
+        return call_user_func_array([static::single(), $name], $arguments);
+    }
 }
