d25558efda4dba9d51e4cda5926f479f5cffc9ec
houdunwang <2300071698@qq.com>
2018-07-29 17:12:18 +0800

    修改命名空间

---
+++ src/Build/js/App.php
@@ -0,0 +1,24 @@
+<?php
+/** .-------------------------------------------------------------------
+ * |  Software: [HDPHP framework]
+ * |      Site: www.hdphp.com
+ * |-------------------------------------------------------------------
+ * |    Author: 向军 <2300071698@qq.com>
+ * |    WeChat: aihoudun
+ * | Copyright (c) 2012-2019, www.houdunwang.com. All Rights Reserved.
+ * '-------------------------------------------------------------------*/
+
+namespace Houdunwang\WeChat\Build\js;
+
+use Houdunwang\WeChat\Build\Base;
+
+/**
+ * JSSDK处理
+ * Class App
+ *
+ * @package Houdunwang\WeChat\Build
+ */
+class App extends Base
+{
+    use Basic;
+}
---
+++ src/WeChatProvider.php
@@ -0,0 +1,23 @@
+<?php namespace Houdunwang\WeChat;
+
+use Illuminate\Support\ServiceProvider;
+class WeChatProvider extends ServiceProvider
+{
+    //延迟加载
+    public $defer = true;
+
+    public function boot()
+    {
+        //配置文件
+        $this->publishes([
+            __DIR__.'/Config.php' => config_path('hd_wechat.php'),
+        ]);
+    }
+
+    public function register()
+    {
+        $this->app->single('HDWeChat', function () {
+            return new WeChat();
+        });
+    }
+}
